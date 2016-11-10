13991c3be7dd6f9bd82e85adf8b7de016ba33ba1
BTIS <ivasansa@gmail.com>
2016-11-10 18:16:19 +0100

    f

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,5 +9,22 @@ export class AppComponent {
   title = 'ta en panga!';
   items = [1,2,3];
 
+
+  public allowDrop(ev) {
+   ev.preventDefault();
+  }
+
+  public drag(ev) {
+    ev.dataTransfer.setData("text", ev.target.id);
+  }
+
+  public drop(ev) {
+    ev.preventDefault();
+    var data = ev.dataTransfer.getData("text");
+    ev.target.appendChild(document.getElementById(data));
+  }
+  public click(){
+    alert("hola");
+  }
 }
 
